a78ed88ad628a1702fcc559e7b75bf2d1d40df71
Amogh Nagalla <amoghnagalla@Amoghs-MacBook-Pro.local>
2017-12-28 22:45:23 -0800

    slider

---
+++ slider.swift
@@ -0,0 +1,17 @@
+let slider = Slider()
+slider.attributedTextForFraction = { fraction in
+    let formatter = NumberFormatter()
+    formatter.maximumIntegerDigits = 3
+    formatter.maximumFractionDigits = 0
+    let string = formatter.string(from: (fraction * 500) as NSNumber) ?? ""
+    return NSAttributedString(string: string)
+}
+slider.setMinimumLabelAttributedText(NSAttributedString(string: "0"))
+slider.setMaximumLabelAttributedText(NSAttributedString(string: "500"))
+slider.fraction = 0.5
+slider.shadowOffset = CGSize(width: 0, height: 10)
+slider.shadowBlur = 5
+slider.shadowColor = UIColor(white: 0, alpha: 0.1)
+slider.contentViewColor = UIColor(red: 78/255.0, green: 77/255.0, blue: 224/255.0, alpha: 1)
+slider.valueViewColor = .white
+view.addSubview(slider)
